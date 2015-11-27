ed34abdb6727ab3d64ce81d00ecec634fbbb2fad
Peter Mannel-Wiedemann <peter.mannel@gmail.com>
2015-11-27 20:03:49 +0100

    initial commit

---
+++ aem-manager-osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  aem-manager-osx
+//
+//  Created by Peter Mannel-Wiedemann on 27.11.15.
+//  
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ aem-manager-osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aem-manager-osx
+//
+//  Created by Peter Mannel-Wiedemann on 27.11.15.
+//  
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
